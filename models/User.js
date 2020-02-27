7e51262ca462def6f219a82d151a8407017688f7
Lorran Sutter <sutterlorran@gmail.com>
2020-02-27 12:24:29 -0500

    Include initial models

---
+++ models/User.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+        },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+        },
+    password: {
+        type: String,
+        required: true,
+    },
+    assets: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Asset'
+        }
+    ],
+    wishlist: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Asset'
+        }
+    ]
+});
+
+module.exports = mongoose.model('User', UserSchema);
