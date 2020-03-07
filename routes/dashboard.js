1e9495f57f66f4bc6c7cb7afe266e61010627238
Lorran Sutter <sutterlorran@gmail.com>
2020-03-06 20:38:09 -0500

    Uncomment routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,18 +8,18 @@ const dashboard_controller = require('../controllers/dashboardController');
 router.get('/', auth, dashboard_controller.dashboard_index);
 
 /* GET dashboard profile page. */
-router.get('/', auth, dashboard_controller.dashboard_profile);
+// router.get('/', auth, dashboard_controller.dashboard_profile);
 
 /* GET dashboard wishlist page. */
-router.get('/', auth, dashboard_controller.dashboard_wishlist);
+// router.get('/', auth, dashboard_controller.dashboard_wishlist);
 
 /* GET dashboard assets page. */
-router.get('/', auth, dashboard_controller.dashboard_assets);
+// router.get('/', auth, dashboard_controller.dashboard_assets);
 
 /* GET dashboard marketplace page. */
-router.get('/', auth, dashboard_controller.dashboard_marketplace);
+// router.get('/', auth, dashboard_controller.dashboard_marketplace);
 
 /* GET dashboard transaction history page. */
-router.get('/', auth, dashboard_controller.dashboard_transactions);
+// router.get('/', auth, dashboard_controller.dashboard_transactions);
 
 module.exports = router;
